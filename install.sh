8e187c061e852d65300507223ae766cd30ad4de7
sodipe <98396909+so-dipe@users.noreply.github.com>
2023-10-11 13:16:24 +0000

    reverse changes

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/opt/render/project/src/.venv/bin/python3.7 -m pip install --upgrade pip
+# /opt/render/project/src/.venv/bin/python3.7 -m pip install --upgrade pip
 
 # Upgrade Python to version 3.10
 apt-get update
